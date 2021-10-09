ed29aeaa0629c4213c37afa2212bad6e6740fb71
Ronald Arceño <badsnalds@gmail.com>
2021-10-09 09:29:02 +0800

    default codes

---
+++ index.php
@@ -0,0 +1,144 @@
+<!doctype>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title> E-LEARNING </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/svgs/WVSU.svg">
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body data-topbar="light" data-layout="horizontal">
+
+<!-- Begin page -->
+<div id="layout-wrapper">
+    <header id="page-topbar" class="">
+        <div class="navbar-header" style="height: 85px">
+            <div class="d-flex container-fluid">
+                <!--logo-->
+                <div class="navbar-brand-box">
+                    <a href="index.php" class="logo logo-dark">
+                        <span class="logo-sm">
+                            <img src="assets/svgs/WVSU.svg" alt="" height="60">
+                        </span>
+                        <span class="logo-lg">
+                            <img src="assets/svgs/WVSU.svg" alt="" height="75">
+                        </span>
+                    </a>
+                </div>
+                <div class="dropdown d-inline-block d-lg-none">
+                    <div class="topnav">
+                        <div class="container-fluid">
+                            <nav class="navbar navbar-light navbar-expand-lg topnav-menu">
+                                <div class="collapse navbar-collapse" id="topnav-menu-content">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item  item-hovered">
+                                            <a class="nav-link" href="#">
+                                                <i class="dripicons-user-group mr-1 align-middle"></i>
+                                                <span class="align-middle">
+                                                    Student
+                                                </span>
+                                            </a>
+                                        </li>
+
+                                        <li class="nav-item  item-hovered">
+                                            <a class="nav-link" href="#">
+                                                <i class="dripicons-user-group mr-1 align-middle"></i>
+                                                <span class="align-middle">
+                                                    Student
+                                                </span>
+                                            </a>
+                                        </li>
+
+                                        <li class="nav-item  item-hovered">
+                                            <a class="nav-link" href="#">
+                                                <i class="dripicons-user-group mr-1 align-middle"></i>
+                                                <span class="align-middle">
+                                                    Student
+                                                </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-5 d-none d-md-inline-block">
+                <ul class="nav nav-pills nav-justified">
+                    <li class="nav-item item-hovered">
+                        <a class="nav-link" href="#">
+                            <i class="dripicons-user-group  mr-1  align-middle"></i><span >Student</span>
+                        </a>
+                    </li>
+                    <li class="nav-item item-hovered">
+                        <a class="nav-link" href="#">
+                            <i class="dripicons-user mr-1 align-middle"></i> <span >Admin</span>
+                        </a>
+                    </li>
+                    <li class="nav-item item-hovered">
+                        <a class="nav-link " href="#">
+                            <i class="dripicons-pencil  mr-1 align-middle"></i> <span >Register
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn btn-sm mr-2 font-size-16 d-lg-none header-item waves-effect  waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+                    <i class="fa fa-fw fa-bars"></i>
+                </button>
+            </div>
+        </div>
+    </header>
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+        <div class="container"
+
+        </div>
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        WEB-DEVELOPER
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="text-sm-right d-none d-sm-block">
+                            DEVELOPER with <i class="mdi mdi-heart text-danger"></i> BADSNALDS
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- end main content-->
+</div>
+<!-- END layout-wrapper -->
+
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+
+<!-- JAVASCRIPT -->
+<script src="assets/libs/jquery/jquery.min.js"></script>
+<script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/libs/metismenu/metisMenu.min.js"></script>
+<script src="assets/libs/simplebar/simplebar.min.js"></script>
+<script src="assets/libs/node-waves/waves.min.js"></script>
+
+<script src="assets/js/app.js"></script>
+
+</body>
+</html>
